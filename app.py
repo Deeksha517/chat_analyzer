97cfc46db16bb9bd00889558b5e0c510380e1e78
Deeksha Jain <deekshajain517@gmail.com>
2025-03-19 11:05:41 +0530

    Fixed Flask port binding issue

--- app.py
+++ app.py
@@ -74,6 +74,7 @@ def home():
     return render_template('splash.html')
 
 
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
@@ -129,5 +130,5 @@ def logout():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render provides a PORT dynamically
+    port = int(os.environ.get("PORT", 10000))  # Render provides a PORT dynamically
     app.run(host="0.0.0.0", port=port)
